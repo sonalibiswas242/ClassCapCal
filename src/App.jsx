421690e1d90a2d5bc41196cf6d7037ff086a2e04
Rishi Patel <96533361+9RP6@users.noreply.github.com>
2024-12-11 13:04:15 -0800

    Initial commit of React (Vite) project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import FaceDetection from "./FaceDetection";
+
+function App() {
+  return (
+    <div className="App">
+      <FaceDetection />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // This allows access from external devices
+    port: 3000,        // You can specify any port you prefer
+  },
+})
